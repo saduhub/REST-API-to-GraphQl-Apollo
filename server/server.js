aba2887d227c319bceb108eadb987469bb370fda
Jorge Duarte <sadujorge@gmail.com>
2024-03-13 16:24:23 -0700

    App runs loacaly and relies on REST API

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {}
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(routes);
 
